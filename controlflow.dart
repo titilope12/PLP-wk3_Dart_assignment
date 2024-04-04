528724a46b67bc10a5627cd5ac3fa3267ddf939e
titilope12 <133906978+titilope12@users.noreply.github.com>
2024-04-04 11:48:41 +0100

    first commit on control flow in dart

---
+++ controlflow.dart
@@ -0,0 +1,10 @@
+void main() {
+  int val = 8;
+  if (val > 10) {
+    print("Your number is greater than 10");
+  } else if (val == 10) {
+    print("Your number is equal to 10");
+  } else {
+    print("Your number is less than 10");
+  }
+}
